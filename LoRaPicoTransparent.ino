97b4ab53e2c52efc924ee89b3c044f493327c657
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2025-12-15 14:08:44 -0600

    Update LoRaPicoTransparent.ino

--- LoRaPicoTransparent.ino
+++ LoRaPicoTransparent.ino
@@ -25,38 +25,29 @@ SOFTWARE.
 * Código creado por Slam(2025)
 * Github: https://github.com/aayes89
 */
-// LIBRERIAS
+
 #include <SPI.h>
 #include <LoRa.h>
-// CONSTANTES GLOBALES
-#define LORA_SS 5
-#define LORA_RST 6
+
+#define LORA_SS   5
+#define LORA_RST  6
 #define LORA_DIO0 7
-#define LORA_SCK 2
+#define LORA_SCK  2
 #define LORA_MISO 4
 #define LORA_MOSI 3
 
 #define LED_PIN 25
 #define FREQ 433E6
 
-#define MAGIC 0xA5
-#define TYPE_DATA 0x01
+#define MAX_FRAME 255
 
-#define MAX_PAYLOAD 180
-#define LEN_HDR 2
-#define MIN_ETH 60
-
-uint8_t tx_seq = 0;
-// INICIALIZACION DE PARAMETROS
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
 
   Serial.begin(115200);
-  while (!Serial)
-    ;
+  while (!Serial);
 
-  // SPI (defaults OK en Pico)
   SPI.setSCK(LORA_SCK);
   SPI.setMISO(LORA_MISO);
   SPI.setMOSI(LORA_MOSI);
@@ -70,69 +61,33 @@ void setup() {
       delay(200);
     }
   }
-  // COMPORTAMIENTO DE LORA
+
   LoRa.setTxPower(20, PA_OUTPUT_PA_BOOST_PIN);
   LoRa.setSpreadingFactor(8);
   LoRa.setSignalBandwidth(125E3);
-  LoRa.setCodingRate4(6);
-  LoRa.disableCrc();
+  LoRa.setCodingRate4(5);
+  LoRa.enableCrc();              // IMPORTANTE
+  LoRa.setPreambleLength(8);
+  LoRa.setSyncWord(0x12);
 
-  Serial.println("LORA BRIDGE READY");
+  Serial.println("LORA RAW BRIDGE READY");
 }
 
 void loop() {
-  // USB -> LoRa 
-  if (Serial.available() >= LEN_HDR) {
-
-    uint16_t len;
-    Serial.readBytes((uint8_t*)&len, 2);
-    
-    // DESCARTAR PAQUETES BASURA
-    if (len < MIN_ETH || len > MAX_PAYLOAD) {      
-      return;
-    }
-    
-    // PREPARAR BUFER DE LECTURA
-    uint8_t buf[MAX_PAYLOAD];
-    size_t got = Serial.readBytes(buf, len);
-    if (got != len) return;
-    
-    // ENVIAR EL PAQUETE
+  // USB → LoRa
+  while (Serial.available()) {
     LoRa.beginPacket();
-    LoRa.write(MAGIC);
-    LoRa.write(TYPE_DATA);
-    LoRa.write(tx_seq++);
-    LoRa.write(buf, len);
+    while (Serial.available()) {
+      LoRa.write(Serial.read());
+    }
     LoRa.endPacket();
-
-    digitalWrite(LED_PIN, HIGH);
-    delay(8);
-    digitalWrite(LED_PIN, LOW);
   }
 
-
-  // LoRa -> USB 
+  // LoRa → USB
   int p = LoRa.parsePacket();
-  if (p >= 4) {
-
-    if (LoRa.read() != MAGIC) return;
-    if (LoRa.read() != TYPE_DATA) return;
-    
-    //RESERVADO
-    uint8_t seq = LoRa.read(); 
-
-    uint16_t len = p - 3;
-    if (len < MIN_ETH || len > MAX_PAYLOAD) return;
-
-    // ENVIAR PREFIJO DE LONGITUD
-    Serial.write((uint8_t*)&len, 2);
-
+  if (p) {
     while (LoRa.available()) {
       Serial.write(LoRa.read());
     }
-
-    digitalWrite(LED_PIN, HIGH);
-    delay(20);
-    digitalWrite(LED_PIN, LOW);
   }
 }
